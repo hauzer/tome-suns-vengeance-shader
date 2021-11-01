52e9c239d0a127000373497c853b89c0d08eb30c
Никола "hauzer" Вукосављевић <hauzer.nv@gmail.com>
2021-11-01 18:58:06 +0100

    Cleaned up long lines

--- hooks/load.lua
+++ hooks/load.lua
@@ -1,36 +1,34 @@
 class:bindHook('ToME:birthDone', function(self)
-    if not game.addons then
-        game.addons = {}
-    end
+    game.addons = game.addons or {}
+    game.addons.suns_vengeance_shader = game.addons.suns_vengeance_shader or {}
+    local addon = game.addons.suns_vengeance_shader
 
-    if not game.addons.suns_vengeance_shader then
-        game.addons.suns_vengeance_shader = {}
-    end
-
-    game.addons.suns_vengeance_shader.base_activate = game.player.tempeffect_def.EFF_SUN_VENGEANCE.activate
-    game.player.tempeffect_def.EFF_SUN_VENGEANCE.activate = function(self, eff)
+    local EFF_SUN_VENGEANCE = game.player.tempeffect_def.EFF_SUN_VENGEANCE
+    
+    addon.base_activate = EFF_SUN_VENGEANCE.activate
+    EFF_SUN_VENGEANCE.activate = function(self, eff)
         eff.suns_vengeance_shader_id = self:addTemporaryValue('suns_vengeance_shader', 1)
-        if game.addons.suns_vengeance_shader.base_activate then
-            return game.addons.suns_vengeance_shader.base_activate(self, eff)
+        if addon.base_activate then
+            return addon.base_activate(self, eff)
         end
     end
 
-    game.addons.suns_vengeance_shader.base_deactivate = game.player.tempeffect_def.EFF_SUN_VENGEANCE.deactivate
-    game.player.tempeffect_def.EFF_SUN_VENGEANCE.deactivate = function(self, eff)
+    addon.base_deactivate = EFF_SUN_VENGEANCE.deactivate
+    EFF_SUN_VENGEANCE.deactivate = function(self, eff)
         self:removeTemporaryValue('suns_vengeance_shader', eff.suns_vengeance_shader_id)
-        if game.addons.suns_vengeance_shader.base_deactivate then
-            return game.addons.suns_vengeance_shader.base_deactivate(self, eff)
+        if addon.base_deactivate then
+            return addon.base_deactivate(self, eff)
         end
     end
 
-    game.addons.suns_vengeance_shader.base_updateMainShader = game.player.updateMainShader
+    addon.base_updateMainShader = game.player.updateMainShader
     game.player.updateMainShader = function(self)
-        local retval = game.addons.suns_vengeance_shader.base_updateMainShader(self)
+        local base_return = addon.base_updateMainShader(self)
 
         if game.fbo_shader and self:attr('suns_vengeance_shader') then
             game.fbo_shader:setUniform('colorize', {1, 0.3, 0, 0.3})
         end
 
-        return retval
+        return base_return
     end
 end)
